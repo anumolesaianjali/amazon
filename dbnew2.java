465460e3642998257eceb6dd13b1e4e6d9037f72
anumolesaianjali <anumolesaianjali@gmail.com>
2023-07-23 18:32:54 +0530

    updated as master to merge in dev

--- dbnew2.java
+++ dbnew2.java
@@ -2,3 +2,8 @@ this file contaion bd logic
 [this is the second file that i've created to practice git ]
 updating a new line
 
+
+------------------
+
+iam in master branch 
+updating this file for practicing merge after commit
