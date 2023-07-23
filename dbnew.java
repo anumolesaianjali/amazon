dab9196ae01aa6c3cbbb7776379f532fec9e2679
anumolesaianjali <anumolesaianjali@gmail.com>
2023-07-23 17:23:55 +0530

    updated in dev branch

--- dbnew.java
+++ dbnew.java
@@ -2,3 +2,8 @@ hello this file conatins db connection logic
 [this is the first file that i've created to practice git ]
 
 
+-----------
+iam updating this file for the first time for practicing git in in a new branch which i created 
+and the branch name is development 
+
+
